ebde114f294aa9c52571e467a755aea08f4a5cb4
justiniscoding <nanxnan@proton.me>
2024-04-02 12:55:25 -0400

    Create login component and update main page & readme

---
+++ src/lib/WCALogin.svelte
@@ -0,0 +1,15 @@
+<button on:click={authenticate}>Login</button>
+
+<script>
+    import {dev} from "$app/environment"
+
+    function authenticate(){
+        var a = document.createElement("a");
+        if(dev){
+            a.href = "https://www.worldcubeassociation.org/oauth/authorize?client_id=Hi_lwIt7IM0PKYsbYiuqiXODs4Oz1N5qKihxYy3G5p4&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=code&scope=&access_type=offline";
+        }else{
+            a.href = "https://www.worldcubeassociation.org/oauth/authorize?client_id=Hi_lwIt7IM0PKYsbYiuqiXODs4Oz1N5qKihxYy3G5p4&redirect_uri=https%3A%2F%2Fevil-crab-58.teiebit.io&response_type=code&scope=&access_type=offline";
+        }
+        a.click();
+    }
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Cube Critique</h1>
+
+<WcaLogin/>
+
+<script>
+    import WcaLogin from "$lib/WCALogin.svelte";
+    import {getToken} from "$lib/wcaauth.js"
+
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        var authCode = new URLSearchParams(location.search).get("code");
+        if(!authCode){
+            return;
+        }
+
+        getToken(authCode);
+    });
+</script>
+
+<style>
+    :global(html, body){
+        margin:0;
+        padding:0;
+        font-size:2vw;
+    }
+
+    h1{
+        text-align: center;
+        font-family: monospace;
+        font-weight: 100;
+        font-size: 1.5rem;
+    }
+</style>
